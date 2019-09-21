03a7945a4c5d593a0e99d945d192b71cb8d16b8f
elkingutierrex <elkin.gutierrex@gmail.com>
2019-09-21 15:50:00 -0500

    Tutotial 58 - Parte 2

---
+++ MyVet.Web/Data/Entities/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Data.Entities        
+{
+    public class DataContext : DbContext
+    {
+        public DataContext( DbContextOptions<DataContext>options ) : base(options)
+        {
+
+        }
+
+        public DbSet<Owner> Owners { get; set; }
+    }
+}
---
+++ MyVet.Web/Data/Entities/Owner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Data.Entities
+{
+    public class Owner
+    {
+        public  int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public int Document { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display (Name = "Fist Name")]
+        public int FristName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Last Name")]
+        public int LastName { get; set; }
+
+        [MaxLength(20)]
+        [Display(Name = "Fixed Phone")]
+        public int FixedPhone { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        [Display(Name = "Cell Phone")]
+        public int CellPhone { get; set; }
+            
+        [MaxLength(100)]
+        public int Address { get; set; }
+
+        [Display(Name = "Owner")]
+        public string FullName => $"{FristName} {LastName}";
+
+        [Display(Name = "Owner")]
+        public string FullNameWithDocument => $"{FristName} {LastName} - {Document}";
+    }
+}
